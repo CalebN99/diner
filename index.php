19ea37e265970a21e35c05024ef0a83d44bdc927
Caleb Norris <norrisc129@gmail.com>
2022-05-02 16:37:05 -0400

    Updated routes and views

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+session_start();
+
 //Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -32,8 +34,25 @@ $f3->route('GET /order', function () {
 });
 
 $f3->route('POST /order2', function () {
+    var_dump($_POST);
+    $_SESSION['food'] = $_POST['food'];
+    $_SESSION['meal'] = $_POST['meal'];
+
     $view = new Template();
     echo $view->render('views/orderForm2.html');
+});
+
+$f3->route('POST /summary', function () {
+
+    if(empty($POST['conds'])) {
+        $conds = 'none selected';
+    } else {
+        $conds = implode(", ", $_POST['conds']);
+    }
+    $_SESSION['conds'] = $conds;
+
+    $view = new Template();
+    echo $view->render('views/orderSummary.html');
 });
 
 //Run f3
